741a5d1981700a2f01d7e7cae71f806a1d06bb31
Zoe <hi@zoe.im>
2021-05-31 17:04:20 +0800

    :tada: add utils for core

---
+++ server/core/core_user.go
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021 wellwell.work, LLC by Zoe
+ *
+ * Licensed under the Apache License 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package core
+
+type User struct {
+	Model `json:"model,omitempty" yaml:"model"`
+
+	Account  string `json:"account,omitempty" yaml:"account"`
+	Token    string `json:"token,omitempty" yaml:"token"`
+	IsAdmin  bool   `json:"is_admin,omitempty" yaml:"is_admin"`
+	External string `json:"external,omitempty" yaml:"external"`
+}
